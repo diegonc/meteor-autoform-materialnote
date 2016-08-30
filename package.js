8db91c66fd8a5e635056476826750e2671d04ba7
Diego Nieto Cid <dnietoc@gmail.com>
2016-08-30 20:20:26 -0300

    Start v1.2.1_4

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'diegonc:autoform-materialnote',
   summary: 'Materialnote editor for aldeed:autoform',
-  version: '1.2.1_3',
+  version: '1.2.1_4',
   git: 'https://github.com/diegonc/meteor-autoform-materialnote'
 });
 
